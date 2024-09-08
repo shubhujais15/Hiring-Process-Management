6d91de9e0ebd7b074f7baae35fbecf4434de29f4
shubhujais15 <shubhujaisedu15@gmail.com>
2024-09-08 20:41:19 +0530

    admin-login path created

--- server/hiring_dashboard/applicants/urls.py
+++ server/hiring_dashboard/applicants/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from applicants.views import ApplicantViewSet
+from applicants.views import ApplicantViewSet,admin_login
 
 router = DefaultRouter()
 router.register(r'applicants', ApplicantViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin-login/', admin_login, name='admin_login'),
 ]
