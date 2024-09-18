89fa5b9c682f2bf3e86fe6dc6d3f52cfc0476b6b
shubhujais15 <shubhujaisedu15@gmail.com>
2024-09-19 00:14:37 +0530

    added welcome path

--- server/hiring_dashboard/applicants/urls.py
+++ server/hiring_dashboard/applicants/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import applicants_list_create, applicant_detail, admin_login
+from .views import applicants_list_create, applicant_detail, admin_login, welcome
 
 urlpatterns = [
+    path('', welcome, name='welcome'),
     path('applicants/', applicants_list_create, name='applicants_list_create'),
     path('applicants/<int:pk>/', applicant_detail, name='applicant_detail'),
-    path('admin-login/', admin_login, name='admin_login'),
+    path('admin-login/', admin_login, name='admin_login')
 ]
