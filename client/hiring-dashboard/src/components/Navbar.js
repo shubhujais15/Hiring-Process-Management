abae367f33c03e0810a886032439a53aacf24404
shubhujais15 <shubhujaisedu15@gmail.com>
2024-09-18 23:33:35 +0530

    updated tailwindcss

--- client/hiring-dashboard/src/components/Navbar.js
+++ client/hiring-dashboard/src/components/Navbar.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Navbar = ({ isLoggedIn, onLogout, isSidebarOpen }) => {
   return (
-    <nav className={`bg-blue-300 p-4 flex justify-between items-center rounded-lg ml-2 mt-1 transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-0'}`}>
-      <div>
+    <nav
+      className={`bg-blue-300 p-4 flex justify-between items-center transition-all duration-300 mb-4 mx-1 rounded-lg
+      w-full fixed top-0 z-10 shadow-md ${isSidebarOpen ? 'lg:pl-64' : ''}`}
+    >
+      {/* Logo section */}
+      <div className="flex items-center">
         <img
           src="https://coloredcow.com/wp-content/themes/ColoredCow/dist/img/logo.png"
           alt="Logo"
@@ -11,6 +15,7 @@ const Navbar = ({ isLoggedIn, onLogout, isSidebarOpen }) => {
         />
       </div>
 
+      {/* Logout button for logged-in users */}
       {isLoggedIn && (
         <button
           onClick={onLogout}
