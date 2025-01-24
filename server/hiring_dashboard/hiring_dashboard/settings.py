89cffa11431417dfa117ed9e719546b24d8efb92
shubhujais15 <shubhujaisedu15@gmail.com>
2025-01-24 11:06:55 +0530

    create vercel.json and requirements.txt

--- server/hiring_dashboard/hiring_dashboard/settings.py
+++ server/hiring_dashboard/hiring_dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^2x_%fy_l$o13ss_85+tq$@brk8#iiinqyjflvl&&lu+cu8^jp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
