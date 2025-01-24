b3c8432487db8b6f9722e7350911cda92652a7ee
shubhujais15 <shubhujaisedu15@gmail.com>
2025-01-24 12:21:33 +0530

    updated wsgi

--- server/hiring_dashboard/hiring_dashboard/wsgi.py
+++ server/hiring_dashboard/hiring_dashboard/wsgi.py
@@ -5,7 +5,7 @@ from django.core.wsgi import get_wsgi_application
 
 try:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hiring_dashboard.settings')
-    application = get_wsgi_application()
+    app = get_wsgi_application()
 except Exception as e:
     sys.stderr.write(f"WSGI error: {e}\n")
     raise
